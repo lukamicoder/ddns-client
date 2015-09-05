afe2369e3a5b3f6a1e970e74565a2d5c7b6f7f56
lukamicoder <lukamicoder@gmail.com>
2015-09-05 15:53:36 -0700

    Upgraded service library.

--- ddns-client.go
+++ ddns-client.go
@@ -1,28 +1,26 @@
 package main
 
 import (
-	"bitbucket.org/kardianos/service"
+	"github.com/kardianos/service"
 	"errors"
-	"fmt"
 	"github.com/lukamicoder/ini-parser"
 	"io/ioutil"
 	"math/rand"
 	"net"
 	"net/http"
-	"os"
 	"regexp"
 	"strings"
 	"time"
+	"log"
+	"os"
 )
 
 var (
 	interval = 3600
 	services []IDdns
-	log service.Logger
+	logger service.Logger
 
 	regex = regexp.MustCompile("(?m)[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")
-	logLevel = "info"
-	exit = make(chan struct {})
 )
 
 var urls = []string{
@@ -57,71 +55,75 @@ type IDdns interface {
 	getDomain() string
 }
 
-func main() {
-	var name = "ddns-client"
-	var displayName = "DDNS Client"
-	var desc = "Dynamic DNS Client."
+type program struct {
+	exit chan struct{}
+}
 
-	rand.Seed(time.Now().UTC().UnixNano())
+func (p *program) Start(s service.Service) error {
+	p.exit = make(chan struct{})
+	go p.run()
+	return nil
+}
 
-	var s, err = service.NewService(name, displayName, desc)
-	if err != nil {
-		fmt.Printf("Unable to start: %s\n", err)
-		return
+func (p *program) run() error {
+	update()
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			update()
+		case <-p.exit:
+			ticker.Stop()
+			return nil
+		}
 	}
-	log = s
+}
+
+func (p *program) Stop(s service.Service) error {
+	logger.Info("Stopping service...")
+	close(p.exit)
+	return nil
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	
+	svcConfig := &service.Config{
+		Name:       "ddns-client",
+		DisplayName: "DDNS Client",
+		Description: "Dynamic DNS Client.",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	err = loadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if len(os.Args) > 1 {
-		var err error
-		verb := os.Args[1]
-		switch verb {
-		case "install":
-			err = s.Install()
-			if err != nil {
-				fmt.Printf("Failed to install: %s\n", err)
-			} else {
-				fmt.Printf("Service installed.\n")
-			}
-		case "remove":
-			err = s.Remove()
-			if err != nil {
-				fmt.Printf("Failed to remove: %s\n", err)
-			} else {
-				fmt.Printf("Service removed.\n")
-			}
-		case "start":
-			err = s.Start()
-			if err != nil {
-				fmt.Printf("Failed to start: %s\n", err)
-			} else {
-				fmt.Printf("Service started.\n")
-			}
-		case "stop":
-			err = s.Stop()
-			if err != nil {
-				fmt.Printf("Failed to stop: %s\n", err)
-			} else {
-				fmt.Printf("Service stopped.\n")
-			}
+		err := service.Control(s, os.Args[1])
+		if err != nil {
+			log.Printf("Valid actions: %q\n", service.ControlAction)
+			log.Fatal(err)
 		}
-
+		
 		return
 	}
-
-	err = s.Run(func() error {
-		err = loadConfig()
-		if err != nil {
-			return err
-		}
-
-		go runTicker()
-		return nil
-	}, func() error {
-		stopTicker()
-		return nil
-	})
+	
+	err = s.Run()
 	if err != nil {
-		s.Error(err.Error())
+		logger.Error(err)
 	}
 }
 
@@ -143,21 +145,16 @@ func loadConfig() error {
 		var name = section.Name
 
 		if name == "settings" {
-			logLevel, _ = config.GetString(name, "loglevel")
-			if logLevel != "info" && logLevel != "error" && logLevel != "nolog" {
-				logLevel = "info"
-				log.Error("Incorrect loglevel in config file: %v", logLevel)
-			}
 			interval, err = config.GetInt(name, "interval")
 			if err != nil {
-				logMessage("error", err.Error())
+				logger.Errorf("%s - %s", name, err)
 			}
 			continue
 		}
 
 		t, err := config.GetString(name, "type")
 		if err != nil {
-			logMessage("error", err.Error())
+			logger.Error(err)
 			continue
 		}
 		switch strings.ToLower(t) {
@@ -166,18 +163,19 @@ func loadConfig() error {
 			service.Name = name
 			service.Domain, err = config.GetString(name, "domain")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			_, err := net.LookupHost(service.getDomain())
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 
 			service.Password, err = config.GetString(name, "password")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			services = append(services, service)
@@ -187,23 +185,23 @@ func loadConfig() error {
 			service.Name = name
 			service.Domain, err = config.GetString(name, "domain")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			_, err := net.LookupHost(service.getDomain())
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 
 			service.Account, err = config.GetString(name, "account")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			service.Password, err = config.GetString(name, "password")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			services = append(services, service)
@@ -212,23 +210,23 @@ func loadConfig() error {
 			service.Name = name
 			service.Domain, err = config.GetString(name, "domain")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			_, err := net.LookupHost(service.getDomain())
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 
 			service.Account, err = config.GetString(name, "account")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			service.Password, err = config.GetString(name, "password")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			services = append(services, service)
@@ -237,18 +235,18 @@ func loadConfig() error {
 			service.Name = name
 			service.Domain, err = config.GetString(name, "domain")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			_, err := net.LookupHost(service.getDomain())
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 
 			service.Token, err = config.GetString(name, "token")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			services = append(services, service)
@@ -257,18 +255,18 @@ func loadConfig() error {
 			service.Name = name
 			service.Domain, err = config.GetString(name, "domain")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			_, err := net.LookupHost(service.getDomain())
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 
 			service.Token, err = config.GetString(name, "token")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			services = append(services, service)
@@ -278,17 +276,17 @@ func loadConfig() error {
 			service.Name = name
 			service.Domain, err = config.GetString(name, "domain")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			_, err := net.LookupHost(service.getDomain())
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			service.Token, err = config.GetString(name, "token")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			services = append(services, service)
@@ -297,17 +295,17 @@ func loadConfig() error {
 			service.Name = name
 			service.Domain, err = config.GetString(name, "domain")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			_, err := net.LookupHost(service.getDomain())
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			service.Password, err = config.GetString(name, "password")
 			if err != nil {
-				logMessage("error", "%s - %s", name, err)
+				logger.Errorf("%s - %s", name, err)
 				continue
 			}
 			services = append(services, service)
@@ -321,41 +319,6 @@ func loadConfig() error {
 	return nil
 }
 
-func logMessage(level string, format string, a ...interface{}) {
-	if logLevel == "nolog" {
-		return
-	}
-
-	if logLevel == "info" && level == "info" {
-		log.Info(format, a...)
-		return
-	}
-
-	if level == "error" {
-		log.Error(format, a...)
-	}
-}
-
-func runTicker() {
-	update()
-	ticker := time.NewTicker(time.Duration(interval) * time.Second)
-
-	for {
-		select {
-		case <-ticker.C:
-			update()
-		case <-exit:
-			ticker.Stop()
-			return
-		}
-	}
-}
-
-func stopTicker() {
-	logMessage("info", "Stopping service...")
-	exit <- struct {}{}
-}
-
 func update() {
 	currentIP := getExternalIP()
 	if currentIP == nil {
@@ -365,24 +328,24 @@ func update() {
 	for _, service := range services {
 		addr, err := net.LookupHost(service.getDomain())
 		if err != nil {
-			logMessage("error", "%s - %s", service.getDomain(), err)
+				logger.Errorf("%s - %s", service.getDomain(), err)
 			continue
 		}
 		if len(addr) == 0 || addr[0] == "" {
-			logMessage("error", "%s - Unable to get IP address", service.getDomain())
+			logger.Errorf("%s - Unable to get IP address", service.getDomain())
 			continue
 		}
 
 		registeredIP := addr[0]
 
 		if currentIP.String() == registeredIP {
-			logMessage("info", "%s - No update is necessary", service.getDomain())
+			logger.Infof("%s - No update is necessary", service.getDomain())
 		} else {
 			err := service.updateIP()
 			if err == nil {
-				logMessage("info", "%s - Successfully updated from %s to %s", service.getDomain(), registeredIP, currentIP.String())
+				logger.Infof("%s - Successfully updated from %s to %s", service.getDomain(), registeredIP, currentIP.String())
 			} else {
-				logMessage("error", "%s - %s", service.getDomain(), err)
+				logger.Errorf("%s - %s", service.getDomain(), err)
 			}
 		}
 	}
@@ -391,11 +354,11 @@ func update() {
 func getExternalIP() net.IP {
 	var currentIP net.IP
 	for _, i := range rand.Perm(len(urls)) {
-		url := urls[i]
+		url := "http://" + urls[i]
 
 		content, err := GetResponse(url, "", "")
 		if err != nil {
-			logMessage("error", "%s - %s", url, err)
+			logger.Errorf("%s - %s", url, err)
 			continue
 		}
 

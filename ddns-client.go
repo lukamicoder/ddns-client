27e7ef62503a3e055cd423d4033884e4e1b0c64b
lukamicoder <lukamicoder@gmail.com>
2017-08-27 07:56:22 -0700

    Removed ipv4.ipogre.com

--- ddns-client.go
+++ ddns-client.go
@@ -32,7 +32,6 @@ var urls = []string{
 	"www.ipchicken.com",
 	"ipecho.net/plain",
 	"bot.whatismyipaddress.com",
-	"ipv4.ipogre.com",
 	"smart-ip.net/myip",
 	"checkip.amazonaws.com",
 	"www.checkip.org",
